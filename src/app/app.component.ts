9d89c663d7545923f10e15d98c8eb3801e6f00aa
wangxuguang <nova_wangxuguang@126.com>
2018-07-17 14:55:02 +0800

    增加angular Http模块，模拟数据请求

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { HEROES } from './mock-heroes';
 
 @Component({
   selector: 'app-root',
@@ -8,5 +7,4 @@ import { HEROES } from './mock-heroes';
 })
 export class AppComponent {
   title = 'Tour of Heroes';
-  heroes = HEROES;
 }
